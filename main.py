e4ba6d2b977c6e5c35c32852154331b2ad49b397
flatline-01 <iknowthatiknownothing@tutanota.com>
2023-06-21 17:13:06 +0600

    add tracks download functionality

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-import telebot
 import os
+import re
+import asyncio
+import requests
+from telebot.async_telebot import AsyncTeleBot
+from sclib.asyncio import SoundcloudAPI, Track
+from telebot.types import InputFile
 
 token = os.getenv('TOKEN')
 
-bot = telebot.TeleBot(token=token)
+bot = AsyncTeleBot(token=token)
+
 
 @bot.message_handler(commands=['start'])
-def greet(message):
-    bot.send_message(message.chat.id, 'Hey there. To download some track just enter its url.')
+async def greet(message):
+    await bot.send_message(message.chat.id, 'Hey there. To download some track just enter its url.')
 
 
-bot.infinity_polling()
+@bot.message_handler(func=lambda m: True)
+async def download(message):
+    chat_id = message.chat.id
+    track_url = message.text
+
+    pattern_web = re.compile("https://soundcloud.com/[-a-zA-Z0-9@:%._+~#=]{1,256}/[-a-zA-Z0-9@:%._+~#=]{1,256}")
+    pattern_app = re.compile("https://soundcloud.app.goo.gl/[-a-zA-Z0-9@:%._+~#=]{5}")
+
+    if pattern_web.match(track_url) or pattern_app.match(track_url):
+        if pattern_app.match(track_url):
+            track_url = requests.get(track_url).url
+        await bot.send_message(chat_id, 'Wait a minute please.')
+        track_path = await download_track(track_url)
+        await bot.send_audio(chat_id, InputFile(track_path))
+        os.remove(track_path)
+    else:
+        await bot.send_message(chat_id, 'Sorry, but I can\'t understand you. Send me a track url.')
+
+
+async def download_track(url):
+    api = SoundcloudAPI()
+    track = await api.resolve(url)
+    assert type(track) is Track
+    track_path = f'tracks/{track.title}.mp3'
+    with open(track_path, 'wb+') as file:
+        await track.write_mp3_to(file)
+    return track_path
+
+
+asyncio.run(bot.infinity_polling())
